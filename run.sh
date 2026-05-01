c0bb8bb7b093543829bcc3963871c1c8301ec618
Elif Sanem Ceyhan <270886456+sanerifu@users.noreply.github.com>
2026-05-01 20:10:34 +0300

    Make it compatible with macOS

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-cc sarma.c -o sarma -g3 #-fsanitize=address -fsanitize=leak
+cc sarma.c -std=c11 -o sarma -g3 #-fsanitize=address -fsanitize=leak
 
 if [ $? -eq 0 ]; then
     ./sarma "$@"
